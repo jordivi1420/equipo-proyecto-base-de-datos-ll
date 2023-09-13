bc536eef25a8ea2c3f79834c5f657f75d47b1ca8
jordivi1420 <111588934+jordivi1420@users.noreply.github.com>
2023-09-13 12:59:39 -0500

    Update proyecto_3.sql

--- proyecto_3.sql
+++ proyecto_3.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `taller2` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `taller2`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: taller2
@@ -167,4 +169,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-13 12:35:30
+-- Dump completed on 2023-09-13 12:56:14
