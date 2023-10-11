cdf039f9da3d9ee86aa8ec3aade5df1f3e420566
IGeikerI <141940507+IGeikerI@users.noreply.github.com>
2023-10-11 00:01:38 -0500

    postgreesql

---
+++ postgreesql/sitealmacen/Apps/ventas/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def home(request):
+    return HttpResponse("Bienvenidos, Uniguajira!- Aplicación Ventas")
